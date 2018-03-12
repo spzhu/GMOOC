fac0c732c9c78723984acbac0fc2c9499a923b51
spzhu <943481660@qq.com>
2018-03-12 15:12:49 +0800

    评论布局，js

--- workspace/js/deco-user.js
+++ workspace/js/deco-user.js
@@ -86,7 +86,7 @@ $(function(){
             cache: false,
             type: "POST",
             dataType:'json',
-            url:"/users/update/pwd/",
+            url:"/users/modify_pwd/",
             data:$('#jsResetPwdForm').serialize(),
             async: true,
             success: function(data) {
